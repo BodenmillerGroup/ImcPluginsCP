c61a37774a3e2503a07b3d524064c48b06299155
votti <mcvotti@gmail.com>
2020-10-02 17:21:55 +0200

    Add basic test if modules can be loaded

---
+++ tests/test_correctspilloverapply.py
@@ -0,0 +1,23 @@
+import numpy
+import pytest
+import io
+
+import cellprofiler_core.image
+import cellprofiler_core.measurement
+
+import cellprofiler_core.modules.injectimage
+import cellprofiler_core.object
+import cellprofiler_core.pipeline
+import cellprofiler_core.workspace
+from cellprofiler_core.utilities.core import modules as cpmodules
+
+IMAGE_NAME = "image"
+OUTPUT_IMAGE_F = "outputimage%d"
+
+import plugins.correctspilloverapply as correctspilloverapply
+
+def test_init():
+    x = correctspilloverapply.CorrectSpilloverApply()
+
+
+
---
+++ tests/test_masktobinstack.py
@@ -0,0 +1,20 @@
+import numpy
+import pytest
+import io
+
+import cellprofiler_core.image
+import cellprofiler_core.measurement
+
+import cellprofiler_core.modules.injectimage
+import cellprofiler_core.object
+import cellprofiler_core.pipeline
+import cellprofiler_core.workspace
+from cellprofiler_core.utilities.core import modules as cpmodules
+
+IMAGE_NAME = "image"
+OUTPUT_IMAGE_F = "outputimage%d"
+
+import plugins.masktobinstack as masktobinstack
+
+def test_init():
+    x = masktobinstack.MaskToBinstack()
